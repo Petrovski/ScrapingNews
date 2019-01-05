409dff7bf2bdef729374f9fce2362d0c0cbff951
FORTRESS <alexpetroski@gmail.com>
2019-01-05 09:01:17 -0500

    Minor changes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express"),
-    logger = require("morgan"),
-    mongoose = require("mongoose"),
-    axios = require("axios"),
-    cheerio = require("cheerio"),
-    db = require("./models"),
-    PORT = 3000;
+      logger = require("morgan"),
+      mongoose = require("mongoose"),
+      axios = require("axios"),
+      cheerio = require("cheerio"),
+      db = require("./models"),
+      PORT = 3000;
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // Initialize Express
 var app = express();
@@ -25,7 +29,7 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 // Routes
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function (req, res) {
+app.get("/scrape",  (req, res) => {
     // First, we grab the body of the html with axios
     axios.get("http://www.echojs.com/").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
@@ -62,7 +66,7 @@ app.get("/scrape", function (req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function (req, res) {
+app.get("/articles", (req, res) => {
     // TODO: Finish the route so it grabs all of the articles
 
     // Find all notes in the notes collection
@@ -80,7 +84,7 @@ app.get("/articles", function (req, res) {
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function (req, res) {
+app.get("/articles/:id",(req, res) => {
     // TODO
     // ====
     // Finish the route so it finds one article using the req.params.id,
@@ -107,7 +111,7 @@ app.get("/articles/:id", function (req, res) {
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function (req, res) {
+app.post("/articles/:id", (req, res) => {
     // TODO
     // ====
     // save the new note that gets posted to the Notes collection
@@ -121,18 +125,9 @@ app.post("/articles/:id", function (req, res) {
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
-        .then(function (dbArticle) {
-            // If the User was updated successfully, send it back to the client
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            // If an error occurs, send it back to the client
-            res.json(err);
-        });
-
+        .then(dbArticle => res.json(dbArticle))
+        .catch(err => res.json(err));
 });
 
 // Start the server
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
-});
+app.listen(PORT, () => console.log("App running on port " + PORT + "!"));
